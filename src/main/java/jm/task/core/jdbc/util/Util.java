0c01c351da36d1696e2b52b5f23de789e3f558e8
NeVadim <g-l-y-k@yandex.ru>
2022-11-20 17:23:48 +0300

    v1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection connection = null;
+    private static String url = "jdbc:mysql://localhost:3306/mydb";
+    private static String user = "root";
+    private static String password = "root";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("JDBC Driver error");
+        }
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection Error");
+        }
+        return connection;
+    }
 }
